72049f751c8de90d6a52e6873c8415235c6257e4
Oluwakemi Owolabi <kemiowolabi90@yahoo.com>
2022-06-22 22:36:10 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,9 @@
+let fullName = "My name is Oluwakemi Owolabi";
+let height ="I'm about 5'6ft tall";
+let country = "And i am from Nigeria";
+
+
+
+console.log(fullName);
+console.log(height);
+console.log(country);
